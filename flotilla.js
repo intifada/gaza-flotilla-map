51ebf3cafacc9dfde6c538fd34823d32638fd663
Benjamin Doherty <bjd@pobox.com>
2010-06-09 00:46:41 -0500

    move layer data to separate directory.

--- flotilla.js
+++ flotilla.js
@@ -4,7 +4,7 @@ function init() {
 
   layer = new OpenLayers.Layer.WMS( "OpenLayers WMS", "http://labs.metacarta.com/wms/vmap0?", {layers: 'basic'});
   map.addLayer(layer);
-  layer = new OpenLayers.Layer.MarineTraffic( "Mavi Marmara", "D6FU2-20100531.xml");
+  layer = new OpenLayers.Layer.MarineTraffic( "Mavi Marmara", "data/D6FU2-20100531.xml");
   map.addLayer(layer);
 
   map.setCenter(new OpenLayers.LonLat(0, 0), 0);
