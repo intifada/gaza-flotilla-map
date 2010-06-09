119dbf7e8da9f3fe1795f6169058815bf2fb7464
Benjamin Doherty <bjd@pobox.com>
2010-06-09 00:45:26 -0500

    improved index. styles. application script.

---
+++ flotilla.js
@@ -0,0 +1,12 @@
+function init() {
+  var map, layer;
+  map = new OpenLayers.Map('map');
+
+  layer = new OpenLayers.Layer.WMS( "OpenLayers WMS", "http://labs.metacarta.com/wms/vmap0?", {layers: 'basic'});
+  map.addLayer(layer);
+  layer = new OpenLayers.Layer.MarineTraffic( "Mavi Marmara", "D6FU2-20100531.xml");
+  map.addLayer(layer);
+
+  map.setCenter(new OpenLayers.LonLat(0, 0), 0);
+  map.addControl(new OpenLayers.Control.LayerSwitcher());
+};
