73b9c3b730f8731197a91bca8ff819e604df3609
Benjamin Doherty <bjd@pobox.com>
2010-06-09 20:09:38 -0500

    whitespace.

--- flotilla.js
+++ flotilla.js
@@ -18,9 +18,9 @@ function init() {
     ]
   });
 
-
   layer = new OpenLayers.Layer.WMS( "OpenLayers WMS", "http://labs.metacarta.com/wms/vmap0?", {layers: 'basic'});
   map.addLayer(layer);
+
   layer = new OpenLayers.Layer.MarineTraffic( "Mavi Marmara", "data/D6FU2-20100531.xml");
   map.addLayer(layer);
 
@@ -33,7 +33,6 @@ function init() {
   map.addLayer(layer);
   layer = new OpenLayers.Layer.MarineTraffic( "Defne Y B", "data/T3SX-20100531.xml");
   map.addLayer(layer);
-
 
   map.setCenter(new OpenLayers.LonLat(34, 32), 0);
   map.zoomTo(9);
